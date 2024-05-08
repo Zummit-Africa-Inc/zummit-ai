42df91fe924f608e78ce2cc5b8e5e0a64f56f074
Samson Okunola <smsnmicheal@gmail.com>
2024-05-08 01:56:11 +0100

    feature: added `enquiries`, and `faq`

---
+++ src/features/faq/data.ts
@@ -0,0 +1,38 @@
+export const faqs = [
+	{
+		label: "What is community based learning?",
+		content:
+			"Community-based learning (CBL) is a teaching and learning strategy that integrates meaningful community engagement with instruction and reflection to enrich the learning experience with a greater emphasis on reciprocal learning and reflection. \nIt's about learning by doing and by experiencing things firsthand. For example, instead of just reading about a technology, you are exposed to what it does in real world by community partners who work in these industries.",
+	},
+	{
+		label: "How long is the Program?",
+		content:
+			"It is a 4 months intensive program. You will be provided with a curriculum and guidance during the learning process. \nAfter the 4 months of learning, upon graduation you will remain in the community. The community members get exposed to job and internship opportunities.",
+	},
+	{
+		label: "How often do you run your cohort?",
+		content:
+			"We run cohorts twice a year and our first cohort begins in January. The second cohort begins in July. We encourage you to apply sooner rather than later because many big tech companies are easier to get into when you are younger. If you don't get in now, it'll increase your chances of getting in next time - we will notice your commitment.",
+	},
+	{
+		label: "I have experience already, can I still join?",
+		content:
+			"Yes certainly, the Community fills the swiftly growing need to share real-world Machine Learning Operations best practices from engineers in the field. Webinars and meet ups will also be held to grow the community. \nMembers of the community get hiring preference from our recruiting partners.",
+	},
+	{
+		label:
+			"Does the Zummit Africa program exist for product managers, and other tech fields?",
+		content:
+			"At the moment our focus is to democratize machine learning and software engineering skills across Africa.",
+	},
+	{
+		label: "How much does Zummit Africa charge?",
+		content:
+			"Zummit Africa charges 10% of your salary for the first 18 months after you get your dream job. You are reinvesting in Zummit. There is no risk - you only pay if you get a fantastic job. Paying back your tuition makes it possible for Zummit Africa to support more people like you.",
+	},
+	{
+		label: "What is the application process?",
+		content:
+			"You will go through a test, the test helps us know your skill level as well as your motivation. The skill test is not very technical it just helps us know how motivated you are.",
+	},
+]
