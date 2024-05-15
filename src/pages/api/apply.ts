8377dd7696708e64658dcd8f41c12686cbe5336c
Samson Okunola <smsnmicheal@gmail.com>
2024-05-15 11:33:17 +0100

    chore: form submission fix for prod

--- src/pages/api/apply.ts
+++ src/pages/api/apply.ts
@@ -8,29 +8,39 @@ export default async function handler(
 	res: NextApiResponse<DataResponse>
 ) {
 	try {
+		await prisma.$connect()
 		let response: DataResponse
-		if (req.method !== "POST") {
+		if (req.method === "POST") {
+			const form = await prisma.form.create({
+				data: { ...req.body },
+			})
+			if (!form) {
+				response = {
+					error: true,
+					message: "Form not created",
+				}
+				return res.status(400).json(response)
+			}
+			response = {
+				error: false,
+				message: "Application submitted",
+			}
+			return res.status(201).json(response)
+		} else if (req.method === "GET") {
+			const forms = await prisma.form.findMany({})
+			response = {
+				error: false,
+				message: "all forms retrieved",
+				data: forms,
+			}
+			return res.status(200).json(response)
+		} else {
 			response = {
 				error: true,
 				message: "Method not allowed",
 			}
 			return res.status(405).json(response)
 		}
-		const form = await prisma.form.create({
-			data: { ...req.body },
-		})
-		if (!form) {
-			response = {
-				error: true,
-				message: "Form not created",
-			}
-			return res.status(400).json(response)
-		}
-		response = {
-			error: false,
-			message: "Application submitted",
-		}
-		return res.status(201).json(response)
 	} catch (error: any) {
 		const response: DataResponse = {
 			error: true,
