c32d9afc59c56186ea389ce549e9b9a456fb7325
Samson Okunola <smsnmichea@gmail.com>
2025-05-22 08:04:08 +0100

    chore: updates

--- src/env.d.ts
+++ src/env.d.ts
@@ -13,9 +13,9 @@ type RequiredEnvs = (typeof requiredEnvs)[number]
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv extends Readonly<Record<RequiredEnvs, string>> {
-			BASE_URL: string
-			NEXT_PUBLIC_BASE_URL: string
-			NEXT_PUBLIC_FACEBOOK_PIXEL_ID: string
+			BASE_URL: "https://zummitaibackend-production.up.railway.app"
+			NEXT_PUBLIC_BASE_URL: "https://zummitaibackend-production.up.railway.app"
+			NEXT_PUBLIC_FACEBOOK_PIXEL_ID: "1307744033479340"
 			NEXT_PUBLIC_GANALYTICS_ID: string
 			NEXT_PUBLIC_POSTHOG_KEY: string
 			NEXT_PUBLIC_POSTHOG_HOST: string
